6ce80e21d68a4a78d9ce2cc42ee382efbe12e63b
dmurtari <domenic.murtari@gmail.com>
2013-11-27 19:08:52 -0700

    Initial commit

---
+++ snake.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <math.h>
+
+//  OpenGL with prototypes for glext
+#define GL_GLEXT_PROTOTYPES
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
